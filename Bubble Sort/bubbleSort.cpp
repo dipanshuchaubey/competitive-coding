0283c1f70a0fa457e538b83aebfcebc55f9e6ba3
Dipanshu Chaubey <dipanshuchaubey@gmail.com>
2019-08-12 23:41:15 +0530

    Bubble Sort

---
+++ Bubble Sort/bubbleSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {3, 2, 5, 9, 4, 2, 1, 6, 55, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted Array: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+
+    cout << "\n";
+
+    return 0;
+}
