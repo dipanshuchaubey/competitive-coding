39c436a980bb59bcc8c46d0e86fe0555bc33eb0a
Dipanshu Chaubey <dipanshuchaubey@gmail.com>
2019-08-18 23:30:45 +0530

    Merge Sort

---
+++ Merge Sort/mergeSort.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int left, int middle, int right)
+{
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    int L[n1];
+    int R[n2];
+
+    for (int i = 0; i < n1; i++)
+    {
+        L[i] = arr[left + i];
+    }
+    for (int j = 0; j < n2; j++)
+    {
+        R[j] = arr[middle + 1 + j];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        int middle = left + (right - left) / 2;
+
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+
+        merge(arr, left, middle, right);
+    }
+}
+
+int main()
+{
+    int arr[] = {30, 10, 20, 40, 60, 70, 25, 60};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+
+    cout << "\n";
+
+    return 0;
+}
